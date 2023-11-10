dfd4ffb92288994699da8a4ef066bd7fc62d57e4
Serafin <sfabaco86@gmail.com>
2023-11-10 17:43:39 +0100

    he creado los get y set de los atributos con metodo de validacion y mensaje de error

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
@@ -5,4 +5,37 @@ public class Rey {
     private Color color;
     private Posicion posicion;
     private int totalMovimientos;
+
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if (color==null){
+            throw new NullPointerException("El color no es correcto");
+        }
+        this.color = color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        if (posicion == null){
+            throw new NullPointerException("La posicion no es valida");
+        }
+
+
+        this.posicion = posicion;
+    }
+
+    public int getTotalMovimientos() {
+        return totalMovimientos;
+    }
+
+    public void setTotalMovimientos(int totalMovimientos) {
+        this.totalMovimientos = totalMovimientos;
+    }
 }
