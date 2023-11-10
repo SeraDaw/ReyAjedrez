3083c89ee8239d4ef82980a749a33be0909c6478
Serafin <sfabaco86@gmail.com>
2023-11-10 13:50:13 +0100

    he creado los getter y los setter de las variables fila y columna

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Posicion.java
@@ -5,5 +5,30 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public int getFila() {
+        return fila;
+    }
 
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna != a || columna != b || columna != c ||
+                columna != d || columna != e || columna != f || columna != g ||
+                columna != h || columna != A || columna != B || columna != C ||
+                columna != D || columna != F || columna != G || columna != H ) {
+            throw new IllegalArgumentException();
+        }
+
+        this.columna = columna;
+    }
+
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8 ) {
+            throw new IllegalArgumentException();
+        }
+
+        this.fila = fila;
+    }
 }
