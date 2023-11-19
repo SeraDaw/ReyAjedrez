7e0ad22345524b540fc20d9eff271f73b24bf73c
Serafin <sfabaco86@gmail.com>
2023-11-19 17:40:30 +0100

    He creado el metodo ejecutar opcion de la clase main

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -9,5 +9,32 @@ public class MainApp {
 
     private static Rey rey;
 
+    private static void ejecutarOpcion (int opcion) {
+
+        switch (opcion) {
+
+            case 1:
+                crearReyDefecto();
+                break;
+
+            case 2:
+                crearReyColor();
+                break;
+
+            case 3:
+                mover();
+                break;
+
+            case 4:
+                despedirse();
+
+        }
+    }
+
+    private static void crearReyDefecto() {
+
+        rey = new Rey();
+    }
+
 
 }
