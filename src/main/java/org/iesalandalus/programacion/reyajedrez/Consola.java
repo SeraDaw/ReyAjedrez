9c11d928bf28e3eb374c79855dbfa694bb3cb249
Serafin <sfabaco86@gmail.com>
2023-11-19 14:20:04 +0100

    He creado la clase mostrarMenu en consola.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -5,4 +5,13 @@ public class Consola {
     private Consola () {
 
     }
+
+    public static void mostrarMenu () {
+        System.out.println("Menu de opciones:");
+        System.out.println("1. Crear Rey por defecto.");
+        System.out.println("2. Crear rey (elige color).");
+        System.out.println("3. Mover");
+        System.out.println("4. Salir.");
+
+    }
 }
