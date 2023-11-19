737034fcbf825b684729db6e5f9a393ecf18786d
Serafin <sfabaco86@gmail.com>
2023-11-19 13:55:57 +0100

    He creado el metodo posicion.

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Rey.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Rey {
 
     private Color color;
@@ -8,15 +10,16 @@ public class Rey {
 
      public Rey(){
         setColor(Color.BLANCO);
-        setPosicion(new posicion (1,'e'));
+        setPosicion(new Posicion (1,'e'));
      }
 
      public Rey (Color color){
         if (color.equals(color.BLANCO)){
-                setPosicion(new posicion (1,'e'));
+                setPosicion(new Posicion (1,'e'));
         }else {
-            setPosicion(new posicion (8,'e'));
+            setPosicion(new Posicion (8,'e'));
         }
+
      }
 
 
@@ -52,5 +55,79 @@ public class Rey {
 
     public void setTotalMovimientos(int totalMovimientos) {
         this.totalMovimientos = totalMovimientos;
+    }
+
+    public void mover (Direccion direccion) throws OperationNotSupportedException{
+
+         int nuevaFila=posicion.getFila();
+         char nuevaColumna=posicion.getColumna();
+         totalMovimientos=0;
+
+        if (direccion==null) {
+            throw new NullPointerException("El movimiento se sale del tablero.");
+        } else {
+
+            switch (direccion) {
+
+                case NORTE:
+                    nuevaFila++;
+                    break;
+
+                case NORESTE:
+                    nuevaFila++;
+                    nuevaColumna++;
+                    break;
+                case ESTE:
+                    nuevaColumna++;
+                    break;
+                case SURESTE:
+                    nuevaFila--;
+                    nuevaColumna++;
+                    break;
+                case SUROESTE:
+                    nuevaFila--;
+                    nuevaColumna--;
+                    break;
+                case SUR:
+                    nuevaFila--;
+                    break;
+                case OESTE:
+                    nuevaColumna--;
+                    break;
+                case NOROESTE:
+                    nuevaFila++;
+                    nuevaColumna--;
+                    break;
+                case ENROQUE_CORTO:
+                    if (totalMovimientos>0) {
+                        throw new NullPointerException("El movimiento no es válido");
+                    } else {
+
+                        nuevaColumna++;
+                        nuevaColumna++;
+                    }
+                    break;
+                case ENROQUE_LARGO:
+                    if (totalMovimientos>0) {
+                        throw new NullPointerException("El movimiento no es válido");
+                    } else {
+                        nuevaColumna--;
+                        nuevaColumna--;
+                    }
+            }
+
+
+            Default: setPosicion(new Posicion(nuevaFila, nuevaColumna));
+                     totalMovimientos++;
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Rey{" +
+                "color=" + color +
+                ", posicion=" + posicion +
+                '}';
     }
 }
